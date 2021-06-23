ec6068fd30647e4becd2a1a90c1b832657710068
Aurimas Sakalys <aurimas.sakalys@stud.vgtu.lt>
2021-06-23 23:54:14 +0300

    Add modulo operator into the grammar

--- chapter6/parsing.c
+++ chapter6/parsing.c
@@ -32,7 +32,7 @@ int main(int argc, char** argv) {
   mpca_lang(MPCA_LANG_DEFAULT,
     "                                                            \
       number     : /-?[0-9]+/ ;                                  \
-      operator   : '+' | '-' | '*' | '/' ;                       \
+      operator   : '+' | '-' | '*' | '/' | '%' ;                 \
       expression : <number> | '(' <operator> <expression>+ ')' ; \
       lispy      : /^/ <operator> <expression>+ /$/ ;            \
     ", Number, Operator, Expression, Lispy);
