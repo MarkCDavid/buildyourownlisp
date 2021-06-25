de8f70f906b598ef4ba02caed21566bd139a3445
Aurimas Sakalys <aurimas.sakalys@stud.vgtu.lt>
2021-06-26 00:32:56 +0300

    Add builtin functions

--- chapter10/parsing.c
+++ chapter10/parsing.c
@@ -22,6 +22,8 @@ void add_history(char* history) {}
 #include <editline/history.h>
 #endif
 
+#define LASSERT(args, cond, err) if (!(cond)) { lval_del(args); return lval_err(err); }
+
 typedef struct lval {
   int type;
   long num;
@@ -173,12 +175,68 @@ lval* lval_take(lval* v, int i) {
   return x;
 }
 
+lval* builtin_head(lval* a) {
+  LASSERT(a, a->count == 1, "Function 'head' passed to many arguments!");
+  LASSERT(a, a->cell[0]->type == LVAL_QEXPR, "Function 'head' passed incorrect types!");
+  LASSERT(a, a->cell[0]->count != 0, "Function 'head' passed {}!");
+  
+  lval* v = lval_take(a, 0);
+  while (v->count > 1) { lval_del(lval_pop(v, 1)); }
+  return v;
+}
+
+lval* builtin_tail(lval* a) {
+  LASSERT(a, a->count == 1, "Function 'tail' passed to many arguments!");
+  LASSERT(a, a->cell[0]->type == LVAL_QEXPR, "Function 'tail' passed incorrect types!");
+  LASSERT(a, a->cell[0]->count != 0, "Function 'tail' passed {}!");
+
+  lval* v = lval_take(a, 0);
+  lval_del(lval_pop(v, 0));
+  return v;
+}
+
+lval* builtin_list(lval* a) {
+  a->type = LVAL_QEXPR;
+  return a;
+}
+
+lval* lval_eval(lval* x);
+
+lval* builtin_eval(lval* a) {
+  LASSERT(a, a->count == 1, "Function 'eval' passed too many arguments!");
+  LASSERT(a, a->cell[0]->type == LVAL_QEXPR, "Function 'eval' passed incorrect type!");
+  
+  lval* x = lval_take(a, 0);
+  x->type = LVAL_SEXPR;
+  return lval_eval(x);
+}
+
+lval* lval_join(lval* x, lval* y) {
+  while(y->count) {
+    x = lval_add(x, lval_pop(y, 0));
+  }
+  lval_del(y);
+  return x;
+}
+
+lval* builtin_join(lval* a) {
+  for (int i = 0; i < a->count; i++) {
+    LASSERT(a, a->cell[i]->type == LVAL_QEXPR, "Function 'join' passed incorrect type!");
+  }
+
+  lval* x = lval_pop(a, 0);
+  
+  while(a->count) {
+    x = lval_join(x, lval_pop(a, 0));
+  }
+
+  lval_del(a);
+  return x;
+}
+
 lval* builtin_op(lval* a, char* op) {
   for(int i = 0; i < a->count; i++) {
-    if(a->cell[i]->type != LVAL_NUM) {
-      lval_del(a);
-      return lval_err("Cannot operator on non-number");
-    }
+    LASSERT(a, a->cell[i]->type == LVAL_NUM, "Cannot operate on non-number!");
   }
 
   lval* x = lval_pop(a, 0);
@@ -210,6 +268,17 @@ lval* builtin_op(lval* a, char* op) {
   return x;
 }
 
+lval* builtin(lval* a, char* func) {
+  if (strcmp("list", func) == 0) { return builtin_list(a); }
+  if (strcmp("head", func) == 0) { return builtin_head(a); }
+  if (strcmp("tail", func) == 0) { return builtin_tail(a); }
+  if (strcmp("join", func) == 0) { return builtin_join(a); }
+  if (strcmp("eval", func) == 0) { return builtin_eval(a); }
+  if (strstr("+-/*", func)) { return builtin_op(a, func); }
+  lval_del(a);
+  return lval_err("Unknown Function!");
+}
+
 
 lval* lval_eval_sexpr(lval* v);
 
@@ -237,9 +306,9 @@ lval* lval_eval_sexpr(lval* v) {
     lval_del(f);
     lval_del(v);
     return lval_err("S-expr does not start with a symbol!");
-  }
+  } 
 
-  lval* result = builtin_op(v, f->sym);
+  lval* result = builtin(v, f->sym);
   lval_del(f);
   return result;
 }
@@ -255,12 +324,13 @@ int main(int argc, char** argv) {
 
   mpca_lang(MPCA_LANG_DEFAULT,
     "                                                    \
-      number      : /-?[0-9]+/ ;                         \
-      symbol      : '+' | '-' | '*' | '/' ;              \
-      qexpr : '{' <expr>* '}' ;                          \
-      sexpr : '(' <expr>* ')' ;                          \
-      expr  : <number> | <symbol> | <sexpr> | <qexpr>  ; \
-      lispy       : /^/ <expr>* /$/ ;                    \
+      number : /-?[0-9]+/ ;                              \
+      symbol : \"list\" | \"head\" | \"tail\" | \"join\" \
+             | \"eval\" | '+' | '-' | '*' | '/' ;        \
+      qexpr  : '{' <expr>* '}' ;                         \
+      sexpr  : '(' <expr>* ')' ;                         \
+      expr   : <number> | <symbol> | <sexpr> | <qexpr> ; \
+      lispy  : /^/ <expr>* /$/ ;                         \
     ", Number, Symbol, Qexpr, Sexpr, Expr, Lispy);
 
 
