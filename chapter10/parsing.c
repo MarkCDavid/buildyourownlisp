ca95bac2f60cbff0ebd5433403c2b391b3d4f949
Aurimas Sakalys <aurimas.sakalys@stud.vgtu.lt>
2021-07-02 00:35:42 +0300

    Add len builtin function

--- chapter10/parsing.c
+++ chapter10/parsing.c
@@ -283,9 +283,22 @@ lval* builtin_cons(lval* a) {
   LASSERT(a, a->count == 2, "Function 'cons' passed to many arguments!");
   LASSERT(a, a->cell[1]->type == LVAL_QEXPR, "Function 'cons' passed incorrect types!");
   LASSERT(a, a->cell[1]->count != 0, "Function 'cons' passed {}!");
+  
+  lval* x = lval_pop(a, 0);
+  lval* y = lval_pop(a, 0);
+  lval* v = lval_unpop(y, x);
 
-  lval* v = lval_add_front(a->cell[1], a->cell[0]);
-  free(a);
+  lval_del(a);
+  return v;
+}
+
+lval* builtin_len(lval* a) {
+  LASSERT(a, a->count == 1, "Function 'len' passed to many arguments!");
+  LASSERT(a, a->cell[0]->type == LVAL_QEXPR, "Function 'len' passed incorrect types!");
+  
+  lval* v = lval_num(a->cell[0]->count);
+  lval_del(a);
+
   return v;
 }
 
@@ -296,6 +309,7 @@ lval* builtin(lval* a, char* func) {
   if (strcmp("join", func) == 0) { return builtin_join(a); }
   if (strcmp("eval", func) == 0) { return builtin_eval(a); }
   if (strcmp("cons", func) == 0) { return builtin_cons(a); }
+  if (strcmp("len", func) == 0) { return builtin_len(a); }
   if (strstr("+-/*", func)) { return builtin_op(a, func); }
   lval_del(a);
   return lval_err("Unknown Function!");
@@ -348,7 +362,7 @@ int main(int argc, char** argv) {
     "                                                    \
       number : /-?[0-9]+/ ;                              \
       symbol : \"list\" | \"head\" | \"tail\" | \"join\" \
-             | \"eval\" | \"cons\"                       \
+             | \"eval\" | \"cons\" | \"len\"             \
              | '+' | '-' | '*' | '/' ;                   \
       qexpr  : '{' <expr>* '}' ;                         \
       sexpr  : '(' <expr>* ')' ;                         \
